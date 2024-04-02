c4761d574e58c36c35e56dc3c0a9c158b32d74de
Nicola Maritan <nicolamaritan01@gmail.com>
2024-04-02 20:00:25 +0200

    imagednadataset

--- main.py
+++ main.py
@@ -34,6 +34,26 @@ class DNADataset(Dataset):
         embedding = self.embeddings_dna[idx]
         label = self.labels[idx]
         return embedding, label
+    
+class ImageDNADataset(Dataset):
+    def __init__(self, annotations_file=None, img_dir=None, transform=None, target_transform=None):
+        data_mat = scipy.io.loadmat('data/INSECTS/data.mat')
+        self.embeddings_img = torch.from_numpy(data_mat['embeddings_img']).float()
+        self.embeddings_dna = torch.from_numpy(data_mat['embeddings_dna']).float()
+        self.labels = torch.from_numpy(data_mat['labels']).long()
+        self.species = data_mat['species']
+        self.ids = data_mat['ids']
+
+    def __len__(self):
+        return len(self.embeddings_dna)
+
+    def __getitem__(self, idx):
+        embedding = torch.cat((self.embeddings_img[idx], self.embeddings_dna[idx]))
+        label = self.labels[idx]
+        return embedding, label
 
 image_data = ImageDataset()
 dna_data = DNADataset()
+image_dna_data = ImageDNADataset()
+
+print(image_dna_data[0][0].shape)
