402fc18e87abb710a943f563a85df9b3cb37af1f
Nicola Maritan <nicolamaritan01@gmail.com>
2024-04-02 19:45:19 +0200

    added dnadataset

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils.data import Dataset
 import os
 
-class Image_Dataset(Dataset):
+class ImageDataset(Dataset):
     def __init__(self, annotations_file=None, img_dir=None, transform=None, target_transform=None):
         data_mat = scipy.io.loadmat('data/INSECTS/data.mat')
         self.embeddings_img = torch.from_numpy(data_mat['embeddings_img']).float()
@@ -18,7 +18,22 @@ class Image_Dataset(Dataset):
         embedding = self.embeddings_img[idx]
         label = self.labels[idx]
         return embedding, label
+    
+class DNADataset(Dataset):
+    def __init__(self, annotations_file=None, img_dir=None, transform=None, target_transform=None):
+        data_mat = scipy.io.loadmat('data/INSECTS/data.mat')
+        self.embeddings_dna = torch.from_numpy(data_mat['embeddings_dna']).float()
+        self.labels = torch.from_numpy(data_mat['labels']).long()
+        self.species = data_mat['species']
+        self.ids = data_mat['ids']
 
-data = Image_Dataset()
-for i in range(10):
-    print(data[i])
+    def __len__(self):
+        return len(self.embeddings_dna)
+
+    def __getitem__(self, idx):
+        embedding = self.embeddings_dna[idx]
+        label = self.labels[idx]
+        return embedding, label
+
+image_data = ImageDataset()
+dna_data = DNADataset()
