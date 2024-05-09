a58d471d188fc6985afde111c1e97f5126988dd1
Nicola Maritan <nicolamaritan01@gmail.com>
2024-05-09 14:47:28 +0200

    working training and inference

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class ImageDNADataset(Dataset):
         self.labels = torch.from_numpy(data_mat["labels"][indeces]).long()
         # data_mat['G'] returns a ndarray of type uint16, therefore we convert into int16 before invoking from_numpy
         self.G = torch.from_numpy(data_mat["G"].astype(np.int16)).long()
-        self.genera = torch.empty(self.labels.shape)
+        self.genera = torch.empty(self.labels.shape).long()
         for i in range(indeces.size):
             self.genera[i][0] = self.G[self.labels[i][0] - 1][0] - 1040
 
@@ -56,12 +56,13 @@ class ImageDNADataset(Dataset):
             (self.embeddings_img[idx], self.embeddings_dna[idx])
         ).view(1, 1, -1)
         label = self.labels[idx].item()
+        genera = self.genera[idx].item()
 
         dwt = DWT1DForward(wave="db6", J=1)
         yl, yh = dwt(embedding)
         embedding = torch.cat((yl, yh[0]), dim=0).squeeze(1)
 
-        return embedding, label
+        return embedding, label, genera
 
 
 image_dna_data = ImageDNADataset()
@@ -85,11 +86,16 @@ class Wavelet1DCNN(nn.Module):
 
         # Flatten
         x = x.view(-1, 32 * 319)
+        x_species = x.clone()
+        x_genera = x.clone()
 
-        # Fully connected layers
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        x_species = F.relu(self.fc1(x_species))
+        x_species = F.relu(self.fc2(x_species))
+
+        x_genera = F.relu(self.fc1(x_genera))
+        x_genera = F.relu(self.fc2(x_genera))
+
+        return x_species, x_genera
 
 
 # Test the network with a random input tensor
@@ -110,12 +116,14 @@ test_loader = torch.utils.data.DataLoader(
     test_set, batch_size=batch_size, shuffle=False
 )
 
-inputs, labels = next(iter(training_loader))
+inputs, labels, genera = next(iter(training_loader))
 print(f"Training input batch: {inputs.shape}")
 print(f"Training label batch: {labels.shape}")
-inputs_test, labels_test = next(iter(test_loader))
+print(f"Training genera batch: {genera.shape}")
+inputs_test, labels_test, genera_test = next(iter(test_loader))
 print(f"Test input batch: {inputs_test.shape}")
 print(f"Test label batch: {labels_test.shape}")
+print(f"Test genera batch: {genera.shape}")
 
 # Report split sizes
 print("Training set has {} instances".format(len(training_set)))
@@ -123,46 +131,53 @@ print("Test set has {} instances".format(len(test_set)))
 
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-epochs = 10
+epochs = 20
+
 
 for epoch in range(epochs):  # loop over the dataset multiple times
 
-    running_loss = 0.0
+    running_labels_loss = 0.0
+    running_genera_loss = 0.0
     for i, data in enumerate(training_loader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
+        inputs, labels, genera = data
+        inputs, labels, genera = inputs.to(device), labels.to(device), genera.to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
+        labels_outputs, genera_outputs = model(inputs)
+        labels_loss = criterion(labels_outputs, labels)
+        genera_loss = criterion(genera_outputs, genera)
+        total_loss = labels_loss + genera_loss
+        total_loss.backward()
         optimizer.step()
 
         # print statistics
-        running_loss += loss.item()
+        running_labels_loss += labels_loss.item()
+        running_genera_loss += genera_loss.item()
         print_step = 500
         if i % print_step == print_step - 1:
-            print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / print_step:.3f}")
-            running_loss = 0.0
+            print(f"[{epoch + 1}, {i + 1:5d}] labels loss: {running_labels_loss / print_step:.3f}")
+            print(f"[{epoch + 1}, {i + 1:5d}] genera loss: {running_genera_loss / print_step:.3f}")
+            running_labels_loss = 0.0
+            running_genera_loss = 0.0
 
 print("Finished Training")
 
-correct = 0
-total = 0
+correct_genera = 0
+total_genera = 0
 # since we're not training, we don't need to calculate the gradients for our outputs
 with torch.no_grad():
     for data in test_loader:
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
+        inputs, labels, genera = data
+        inputs, labels, genera = inputs.to(device), labels.to(device), genera.to(device)
         # calculate outputs by running images through the network
-        outputs = model(inputs)
+        labels_outputs, genera_outputs = model(inputs)
         # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        _, predicted_genera = torch.max(genera_outputs.data, 1)
+        total_genera += labels.size(0)
+        correct_genera += (predicted_genera == labels).sum().item()
 
-print(f"Accuracy of the network on the 10000 test inputs: {correct / total}")
+print(f"Accuracy of the network on the 10000 test inputs: {correct_genera / total_genera}")
